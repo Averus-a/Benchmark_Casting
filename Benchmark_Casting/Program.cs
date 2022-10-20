3307c235e7db61f7c718771fc0d2fbe169ffa4b2
Artem <art.saikin@gmail.com>
2022-10-20 20:39:26 +0300

    Add project files.

---
+++ Benchmark_Casting/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+using System.Diagnostics;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Test>();
+    }
+
+    [SimpleJob(runtimeMoniker: RuntimeMoniker.Net60)]
+    public class Test
+    {
+        private readonly List<object?> _data;
+
+        public Test()
+        {
+            var collection = new List<object?>();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    collection.Add(null);
+                }
+                else
+                {
+                    collection.Add(new());
+                }
+            }
+
+            _data = collection;
+        }
+
+        [Benchmark]
+        public void NoCast()
+        {
+            List<object> collection = _data.Where(x => x is not null).ToList()!;
+        }
+
+        [Benchmark]
+        public void Cast()
+        {
+            List<object> collection = _data.Where(x => x is not null).Cast<object>().ToList();
+        }
+    }
+}
